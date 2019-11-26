1d607bb56f4f0671daf90b0d76a2b3c7303cfc76
east91 <east91@outlook.com>
2019-11-25 18:07:14 -0600

    Added several funtions

--- sandbox/src/main/java/com/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/com/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,27 @@ package com.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+
+    double l = 5;
+    System.out.println("Square area with side " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+
+    System.out.println("Rectangle area with sides " + a + " and " + b + " = " + area(a,b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 }
